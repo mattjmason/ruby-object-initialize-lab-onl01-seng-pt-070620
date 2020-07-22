2721ab260a6c3fe7f7981787593593d972f0b622
Matt Mason <masonmj10@gmail.com>
2020-07-22 15:07:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,18 +2,7 @@ class Dog
   
   def initialize(name, breed = "Mutt")
     @name = name
-  end 
-  
-  def initialize(breed)
     @breed = breed
   end 
 
 end 
-
-  
-  def breed=(breed)
-    @breed = breed
-  end 
-  def breed
-    @breed
-  end 
